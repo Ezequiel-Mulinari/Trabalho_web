70862fd72b2758227f52ff30779ab1b984df6be7
Ezequiel-Mulinari <146396883+Ezequiel-Mulinari@users.noreply.github.com>
2024-09-26 19:47:18 -0300

    Delete js directory

--- js/home.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Obtém o e-mail do localStorage
-    const userEmail = localStorage.getItem('userEmail');
-    // Exibe o e-mail na página
-    document.getElementById('userEmail').innerText = userEmail ? userEmail : 'Usuário não identificado';
-  });
-  
